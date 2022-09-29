81829eba7947719240da520a1d1bdc9e0da6b785
Radamés <soyradames@gmail.com>
2022-09-29 08:45:54 -0300

    Challenge solved.

--- index.js
+++ index.js
@@ -25,6 +25,31 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function main() {
+  const ulClassLista = document.querySelector(".lista");
+
+  // Ref: https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+
+  function eliminarElementosLi() {
+    while (ulClassLista.firstChild) {
+      ulClassLista.removeChild(ulClassLista.firstChild)
+    }
+  }
+
+  function agregarElementosLi() {
+    cosasQueAprendimos.forEach(element => {
+      nuevoLiEl = document.createElement("li");
+      textoNuevoLiEl = document.createTextNode(element.tema);
+      nuevoLiEl.append(textoNuevoLiEl);
+      if (element.class != "") {
+        nuevoLiEl.classList.add(element.class);
+      }
+      ulClassLista.append(nuevoLiEl);
+    });
+  }
+
+  eliminarElementosLi();
+  agregarElementosLi();
+}
 
 main();
